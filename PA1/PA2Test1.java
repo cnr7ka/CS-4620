16caa4136dcfc5c4c37c249ee0fda03136dc4c22
cnr7ka <Cnr7ka@virginia.edu>
2017-09-13 10:57:44 -0400

    Add files via upload

---
+++ PA1/PA2Test1.java
@@ -0,0 +1,16 @@
+/**
+ * PA2Test1.java
+ * 
+ * Test the setPixel statement for the PA2 grammar
+ *
+ * CNR 9/12/2017
+ */
+
+import meggy.Meggy;
+
+class PA2Test1 {
+
+    public static void main(String[] id){
+            Meggy.setPixel((byte)0, (byte)0, Meggy.Color.RED );
+    }
+}
